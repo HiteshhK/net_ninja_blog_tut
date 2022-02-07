d624f441d45375256e941ac4ab3cbec123d0a83b
hitesh.kaushik <hitesh.kaushik@globallogic.com>
2022-02-07 22:16:09 +0530

    adding mongoose hooks to user model

--- models/User.js
+++ models/User.js
@@ -16,6 +16,22 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+/** Hooks, function that runs before/after the document operation is performed */
+//fire a function after document is saved
+userSchema.post('save',(doc,next)=>{
+    console.log('New user was created and saved', doc);
+    
+    //need to call this callback otherwise further execution will stop
+    next();
+});
+
+//fire a funtion before doc is saved to db
+userSchema.pre('save',function(next){
+    //we are using function keyword instead of ()=> becasue we need to use this keyword, which will reference to userSchema, i.e before saving the doc state from schema object
+    console.log('User about to be created & saved', this);
+    next();
+});
+
 const User = mongoose.model('User',userSchema);
 
 module.exports = User;
